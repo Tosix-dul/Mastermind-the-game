ae20a25ef8ea405906afd975eeecc394ba61b0ea
KFerdek <kferdek@student.agh.edu.pl>
2025-05-15 23:04:58 +0200

    'KF_edit_1'

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pygame
 import tkinter.messagebox
 
+
+#konwersja inputu na string cyfr
+def Input_Conv (solution): #niegotowe
+    len = 4
+    dict = {"kolor1":"1","kolor2":"2","kolor3":"3","kolor4":"4"}
+    answer = [str(input(f"Wpisz {i + 1}. kolor: ")) for i in range(len)] #tymczasowe, potem bedzie podawane jako parametr funkcji
+    # do dodania: wlasiwa konwersja
+    return answer
+
+
 #Okno gry
 pygame.init()
 running = True
