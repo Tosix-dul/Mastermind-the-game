eea03de4a372107fa1f348e9903eb9876b4e47fc
KFerdek <kferdek@student.agh.edu.pl>
2025-05-17 23:23:31 +0200

    'KF_edit_3'

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def Input_Conv (answer = []): #gotowe, do zmiany dopiero w sprincie 2.
     answer = [dict_color[answer[i]] for i in range(len(answer))]
     return answer
 
+def Is_Correct (conv_answer,solution): #niegotowe
+    corr = ['r' for i in range(len(conv_answer))]
+    mem = [i for i in range(len(conv_answer))]
+    solution_cpy = solution.copy()
+    # sprawdzenie dobrych kolorow
+    for i in range(len(conv_answer)):
+        if solution[i] == conv_answer[i]:
+            corr[i] = 'g'
+            mem.remove(i)
+            solution_cpy.remove(ex[i])
+    return corr
+
+
 
 #Okno gry
 pygame.init()
