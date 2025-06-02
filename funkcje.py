fa378510301898aa8623fb90c9788e3eea61048c
Antonina <tosiadulak2@gmail.com>
2025-06-03 00:59:28 +0200

    Modyfikacja komentarzy i nazwy tkinter w pliku funkcje

--- funkcje.py
+++ funkcje.py
@@ -8,8 +8,8 @@ import sys
 
 #-----------------Przygotowanie do gry------------------
 
-#funjcja okna dialogowego; Poziom 1, Poziom 2, Poziom 3, Wyjdź, “Stwórz swój własny poziom”, Customizacja, Zasady gry
-# #Definicje do przycisków
+#funkcja okna dialogowego; Poziom 1, Poziom 2, Poziom 3, Wyjdź, “Stwórz swój własny poziom”, Customizacja, Zasady gry
+# Definicje do przycisków
 def start_poziom(nr):
     print(f"Uruchamiam poziom {nr}")
     root.destroy()
@@ -29,23 +29,23 @@ def wyjdz():
     sys.exit()
 
 # Główne okno tkinter
-root = tkinter.Tk()
+root = tk.Tk()
 root.title("Mastermind")
 root.geometry("300x400")
 
 # Przyciski
-tkinter.Button(root, text="Poziom 1", command=lambda: start_poziom(1), width=25).pack(pady=5)
-tkinter.Button(root, text="Poziom 2", command=lambda: start_poziom(2), width=25).pack(pady=5)
-tkinter.Button(root, text="Poziom 3", command=lambda: start_poziom(3), width=25).pack(pady=5)
+tk.Button(root, text="Poziom 1", command=lambda: start_poziom(1), width=25).pack(pady=5)
+tk.Button(root, text="Poziom 2", command=lambda: start_poziom(2), width=25).pack(pady=5)
+tk.Button(root, text="Poziom 3", command=lambda: start_poziom(3), width=25).pack(pady=5)
 
-tkinter.Button(root, text="Stwórz swój własny poziom", command=stworz_poziom, width=25).pack(pady=5)
-tkinter.Button(root, text="Customizacja", command=customizacja, width=25).pack(pady=5)
-tkinter.Button(root, text="Zasady gry", command=zasady_gry, width=25).pack(pady=5)
-tkinter.Button(root, text="Wyjdź", command=wyjdz, width=25).pack(pady=10)
+tk.Button(root, text="Stwórz swój własny poziom", command=stworz_poziom, width=25).pack(pady=5)
+tk.Button(root, text="Customizacja", command=customizacja, width=25).pack(pady=5)
+tk.Button(root, text="Zasady gry", command=zasady_gry, width=25).pack(pady=5)
+tk.Button(root, text="Wyjdź", command=wyjdz, width=25).pack(pady=10)
 
 # Start GUI
 root.mainloop()
-------
+#------
 #funkcja losuje kod dla komputera w postaci listy 4 intów
 '''!!konieczna zmiana sposobu losowania żeby dało się regulować ilość kolorów w kodzie!!'''
 def losuj_kod(liczba_kolorow,dlugosc=4):
